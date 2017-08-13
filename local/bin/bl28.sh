8cbe95329ce117a3959c13c2aa86bf522f977945
Paulo Gomes <tuga3d@gmail.com>
2017-08-13 15:39:26 +0100

    changed mesa to work drive

--- local/bin/bl28.sh
+++ local/bin/bl28.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-export LD_LIBRARY_PATH=/opt/mesa-master/x86_64:$LD_LIBRARY_PATH
-export LIBGL_DRIVERS_PATH=/opt/mesa-master/x86_64
-export EGL_DRIVERS_PATH=/opt/mesa-master/x86_64
+export LD_LIBRARY_PATH=/mnt/work/mesa-master/x86_64:$LD_LIBRARY_PATH
+export LIBGL_DRIVERS_PATH=/mnt/work/mesa-master/x86_64
+export EGL_DRIVERS_PATH=/mnt/work/mesa-master/x86_64
 export MESA_GLSL_TO_TGSI_NEW_MERGE=1
 # export MESA_GLSL_CACHE_DISABLE=1
 /mnt/work/software/blender-2.80/blender "$@"
